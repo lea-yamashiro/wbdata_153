b023ebfa1f38b53dbed24552188b36b3e7370dbf
Oliver Sherouse <oliver@oliversherouse.com>
2024-01-08 23:13:44 -0500

    more fetcher tests

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -24,5 +24,47 @@ def test_get_request_content(mocked_fetcher):
         return_value=MockHTTPResponse(value=expected)
     )
     result = mocked_fetcher._get_response_body(url=url, params=params)
-    assert mocked_fetcher.session.get.called_once_with(url=url, params=params)
+    mocked_fetcher.session.get.assert_called_once_with(url=url, params=params)
     assert json.loads(result) == expected
+
+
+@pytest.mark.parametrize(
+    ["url", "params", "response", "expected"],
+    (
+        pytest.param(
+            "http://foo.bar",
+            {"baz": "bat"},
+            [{"page": "1", "pages": "1"}, [{"hello": "there"}]],
+            fetcher.ParsedResponse(
+                rows=[{"hello": "there"}],
+                page=1,
+                pages=1,
+                last_updated=None,
+            ),
+            id="No date",
+        ),
+        pytest.param(
+            "http://foo.bar",
+            {"baz": "bat"},
+            [
+                {"page": "1", "pages": "1", "lastupdated": "01/02/2023"},
+                [{"hello": "there"}],
+            ],
+            fetcher.ParsedResponse(
+                rows=[{"hello": "there"}],
+                page=1,
+                pages=1,
+                last_updated="01/02/2023",
+            ),
+            id="with date",
+        ),
+    ),
+)
+def test_get_response(url, params, response, expected, mocked_fetcher):
+    mocked_fetcher.session.get = mock.Mock(
+        return_value=MockHTTPResponse(value=response)
+    )
+    got = mocked_fetcher._get_response(url=url, params=params)
+    mocked_fetcher.session.get.assert_called_once_with(url=url, params=params)
+    assert got == expected
+    assert mocked_fetcher.cache[(url), (("baz", "bat"),)] == json.dumps(response)
